b0461dee5b58f06dd67640f637733e231946b299
evankiser01 <evan.kiser@bsu.edu>
2024-10-09 18:46:17 -0400

    Change camera position and angle

--- project/ball/ball.gd
+++ project/ball/ball.gd
@@ -1,11 +1,11 @@
 extends RigidBody3D
 
-var _screen_bottom_limit:= -1.0
+var _bottom_limit:= -5.0
 
 func _on_sleeping_state_changed() -> void:
 	emit_signal("projectile_sleeping_state_changed", is_sleeping)
 
 
-func _process(_delta):
-	if position.y > _screen_bottom_limit:
-		queue_free()
+#func _process(_delta):
+	#if global_position.y > _bottom_limit:
+		#queue_free()
