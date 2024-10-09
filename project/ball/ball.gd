b1cad6e7817adc354541d9aaefbb1de9ec83c960
evankiser01 <evan.kiser@bsu.edu>
2024-10-09 15:05:23 -0400

    Give the ball a script
    The ball is remove itself if it falls below the world

---
+++ project/ball/ball.gd
@@ -0,0 +1,11 @@
+extends RigidBody3D
+
+var _screen_bottom_limit: float
+
+func _on_sleeping_state_changed() -> void:
+	emit_signal("projectile_sleeping_state_changed", is_sleeping)
+
+
+func _process(_delta):
+	if position.y > _screen_bottom_limit:
+		queue_free()
