41facb119ee13a1ec3698f56a7bfc1af74d7a7e9
evankiser01 <evan.kiser@bsu.edu>
2024-10-09 20:45:42 -0400

    Create a title screen
    A button on the title screen will load the world and when the player hits 2 targets they will be returned to the title screen

--- project/world/world.gd
+++ project/world/world.gd
@@ -1,14 +1,23 @@
 extends Node3D
 
-var _targets_hit
+var _targets_hit := 0
+
+func _process(_delta: float) -> void:
+	if _targets_hit == 2:
+		$WinLabel.text = "You Win!\nYou will be returned to the Title Screen"
+		await(get_tree().create_timer(5).timeout)
+		get_tree().change_scene_to_file("res://title_screen/title_screen.tscn")
+
 
 func _on_cannon_power_changed() -> void:
 	$PowerLabel.text = "Power : %.d" % $Cannon.power
 	$PowerLabel.modulate = Color.RED
 
+
 func _on_cannon_projectile_amount_changed() -> void:
 	$ProjectilesLeftLabel.text = "Projectiles Left : %.d" % $Cannon.balls_left
 	$ProjectilesLeftLabel.modulate = Color.RED
+
 
 func _on_cannon_horizontal_angle_changed() -> void:
 	$HorizontalAngleLabel.text = "Horizontal Angle : %.d" % (rad_to_deg(-1*$Cannon.angle_horizontal) + 90)
@@ -21,6 +30,7 @@ func _on_cannon_vertical_angle_changed() -> void:
 
 
 func _on_target_1_target_hit() -> void:
+	_targets_hit += 1
 	if _targets_hit == 1:
 		$TargetHitLabel.text = "You hit one Target!"
 	if _targets_hit == 2:
@@ -28,13 +38,8 @@ func _on_target_1_target_hit() -> void:
 
 
 func _on_target_2_target_hit() -> void:
+	_targets_hit += 1
 	if _targets_hit == 1:
 		$TargetHitLabel.text = "You hit one Target!"
 	if _targets_hit == 2:
 		$TargetHitLabel.text = "You hit both Target!"
-	await(get_tree().create_timer(5).timeout)
-	#get_tree().change_scene_to_file("res://title_screen/title_screen.tscn")
-
-func _both_targets_hit() -> void:
-	if _targets_hit == 2:
-		$WinLabel.text = "You Win!\nPress T to return to the Title Screen"
